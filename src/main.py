6562b49255beffed0524ec8226b5650c9999edb2
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-07-12 16:45:47 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -41,10 +41,9 @@ def handle_trending(message):
     previous_message_ids.append(message.message_id)
 
     full_list = trending()
-    bot.send_message(message.chat.id, 'test')
     print(full_list)
     for movie in full_list:
-        caption = f"{movie['title']}  ({movie['year']})\n {movie['rating']} ⭐ \n{movie['url']}"
+        caption = f"{movie['title']} ({movie['year']})\n {movie['rating']} ⭐ \n{movie['url']}"
         image = movie['image']
         bot.send_photo(message.chat.id, image, caption = caption)
 
